0df56879465adaeb88c39459b8b244ac07a29fd1
Runsheng <runsheng.lee@gmail.com>
2015-06-30 09:50:03 +0800

    pep8 modification

--- gbrowse_script/fasta2gff.py
+++ gbrowse_script/fasta2gff.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 from Bio import SeqIO
+
+
 def fasta2gff(fastafile):
-    "open a fatsafile, count the length, then write a gff file for Gbrowse useage"
+    '''open a fatsafile, count the length, then write a gff file for Gbrowse useage'''
     handle = open(fastafile, "rU")
     writer=open(fastafile.replace(".fa",".gff"),"w")
     for record in SeqIO.parse(handle, "fasta"):
@@ -10,5 +12,7 @@ def fasta2gff(fastafile):
         writer.write((name+"\tfasta\tchr\t1\t"+length+"\t.\t.\t.\tName="+name+"\n"))
     writer.close()
     handle.close()
-#example
-#fasta2gff("cb4.fa")
+
+if __name__=="__main__":
+    #example
+    fasta2gff("cb4.fa")
