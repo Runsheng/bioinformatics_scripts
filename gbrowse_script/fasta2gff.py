b917df7837130e98d37cabd996111f0da2fadb09
Runsheng <runsheng.lee@gmail.com>
2015-11-21 10:26:07 +0800

    add command line call for fasta2gff

--- gbrowse_script/fasta2gff.py
+++ gbrowse_script/fasta2gff.py
@@ -1,18 +1,22 @@
-#! /usr/bin/python
+#!/usr/bin/env python
+
+import argparse
 from Bio import SeqIO
-
-
 def fasta2gff(fastafile):
-    '''open a fatsafile, count the length, then write a gff file for Gbrowse useage'''
+    "open a fatsafile, count the length, then write a gff file for Gbrowse useage"
     handle = open(fastafile, "rU")
-    writer=open(fastafile.replace(".fa",".gff"),"w")
+    writer=open(fastafile.replace(".fa","_title.gff"),"w")
     for record in SeqIO.parse(handle, "fasta"):
         name=str(record.id)
         length=str(len(record.seq))
         writer.write((name+"\tfasta\tchr\t1\t"+length+"\t.\t.\t.\tName="+name+"\n"))
     writer.close()
     handle.close()
-
-if __name__=="__main__":
-    #example
-    fasta2gff("cb4.fa")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fasta")
+    args=parser.parse_args()
+    try:
+        fasta2gff(args.fasta)
+    except Exception:
+        print "Error, check biopython install"
