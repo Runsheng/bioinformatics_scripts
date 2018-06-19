5803a85ba6810683eb5746b416c4ca454cb749b5
runsheng <runsheng.lee@gmail.com>
2018-06-19 19:40:14 +0800

    remove fq2fa.py
    Just repalce the script with
    seqtk seq -A foo.fq >foo.fa

--- fq2fa.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-handle = open("AF16-1_S1_L001_R1_001.fastq.gz", "rU")
-output_handle = open("example.fasta", "w")
-
-for record in SeqIO.parse(handle, "fastq") :
-     SeqIO.write(record, output_handle, "fasta")
-handle = open("example.fasta", "rU")
-output_handle.close()
